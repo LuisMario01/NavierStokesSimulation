3744f1044ad33efff075358b7ecc684edaf14ec8
jonathanDuenas <jonathanduenas94@gmail.com>
2018-07-09 00:56:06 -0600

    R and problemtype

---
+++ R/nvs.R
@@ -0,0 +1,118 @@
+#library(bigmemory)
+
+library(Matrix)
+library(DataCombine)
+
+advection <- 2
+force <- c(1, 2, 3)
+density <- 1
+
+element_node <- read.table("example2.dat", skip=grep("Number of elements and nodes:", readLines("example2.dat")), nrows=1)
+
+coordinates <- read.table("example2.dat", skip=grep("Coordinates:", readLines("example2.dat")), nrows=element_node[1, 2])
+
+connection_table <- read.table("example2.dat", skip=grep("Connectivities:", readLines("example2.dat")), nrows=element_node[1, 1])
+
+con <- file("parse.txt", open='r')
+lines <- readLines(con)
+
+k_local_str <- lines[[1]]
+v_local_str <- lines[[2]]
+b_local_str <- lines[[3]]
+
+#k_global <- big.matrix(nrow=element_node[[1]]*6, ncol=element_node[[1]]*6, type="short", backingfile="k_global")
+#b_global <- big.matrix(nrow=(element_node[[1]]*6)^2, ncol = 1, type="short", backingfile="b_global")
+
+k_global <- Matrix(0, nrow=element_node[[1]]*6, ncol=element_node[[1]]*6, sparse = TRUE)
+b_global <- Matrix(0, (nrow=element_node[[1]]*6)^2, ncol=1, sparse = TRUE)
+
+
+dataGroup <- data.frame(ab = c(k_local_str, b_local_str))
+
+from_o <- c("advection", "x1", "x2", "x3", "x4", "y1", "y2", "y3", "y4", "z1", "z2", "z3", "z4", "fx", "fy", "fz", "density", "--", "+-", "a", "b", "c", "d", "e", "f")
+
+start.time <- Sys.time()
+for(i in 1:500){
+#for(i in 1:element_node[[1]]){
+    dot_1 <- coordinates[connection_table[i,2],2:4]
+    dot_2 <- coordinates[connection_table[i,3],2:4]
+    dot_3 <- coordinates[connection_table[i,4],2:4]
+    dot_4 <- coordinates[connection_table[i,5],2:4]
+
+    con_local <- connection_table[i,2:5]
+    
+    a <- min(dot_1[[1]], dot_2[[1]], dot_3[[1]], dot_4[[1]])
+    b <- max(dot_1[[1]], dot_2[[1]], dot_3[[1]], dot_4[[1]])
+    c <- min(dot_1[[2]], dot_2[[2]], dot_3[[2]], dot_4[[2]])
+    d <- max(dot_1[[2]], dot_2[[2]], dot_3[[2]], dot_4[[2]])
+    e <- max(dot_1[[3]], dot_2[[3]], dot_3[[3]], dot_4[[3]])
+    f <- max(dot_1[[3]], dot_2[[3]], dot_3[[3]], dot_4[[3]])
+
+    to_o <- c(advection, dot_1[[1]], dot_2[[1]], dot_3[[1]], dot_4[[1]], dot_1[[2]], dot_2[[2]], dot_3[[2]], dot_4[[2]], dot_1[[3]], dot_2[[3]], dot_3[[3]], dot_4[[3]], force[[1]], force[[2]], force[[3]], density, "+", "-", a, b, c, d, e, f)
+
+    replaces <- data.frame(from = from_o,
+                           to = to_o)
+
+    data <- FindReplace(data = dataGroup, Var = "ab", replaceData = replaces,
+                        from = "from", to = "to", exact=FALSE, vector=TRUE)
+
+    k_local <- matrix(eval(parse(text=paste("c", data[[1]]))), nrow=24, ncol=24)
+    b_local <- matrix(eval(parse(text=paste("c", data[[2]]))), nrow=24, ncol=1)
+    
+    k_local [is.na(k_local)] <- 0
+    
+    k_local_1 <- k_local[1:6,1:6]
+
+    k_global[(1+(con_local[[1]]-1)*6):(6+(con_local[[1]]-1)*6), (1+(con_local[[1]]-1)*6):(6+(con_local[[1]]-1)*6)] =
+        k_global[(1+(con_local[[1]]-1)*6):(6+(con_local[[1]]-1)*6), (1+(con_local[[1]]-1)*6):(6+(con_local[[1]]-1)*6)] + k_local_1
+
+    k_local_2 <- k_local[1:6,7:12]    
+
+    k_global[(1+(con_local[[1]]-1)*6):(6+(con_local[[1]]-1)*6), (1+(con_local[[2]]-1)*6):(6+(con_local[[2]]-1)*6)] =
+        k_global[(1+(con_local[[1]]-1)*6):(6+(con_local[[1]]-1)*6), (1+(con_local[[2]]-1)*6):(6+(con_local[[2]]-1)*6)] + k_local_2
+
+    k_local_3 <- k_local[7:12,1:6]
+
+    k_global[(1+(con_local[[2]]-1)*6):(6+(con_local[[2]]-1)*6), (1+(con_local[[1]]-1)*6):(6+(con_local[[1]]-1)*6)] =
+        k_global[(1+(con_local[[2]]-1)*6):(6+(con_local[[2]]-1)*6), (1+(con_local[[1]]-1)*6):(6+(con_local[[1]]-1)*6)] + k_local_3
+    
+    k_local_4 <- k_local[7:12,7:12]
+
+    k_global[(1+(con_local[[2]]-1)*6):(6+(con_local[[2]]-1)*6), (1+(con_local[[2]]-1)*6):(6+(con_local[[2]]-1)*6)] =
+        k_global[(1+(con_local[[2]]-1)*6):(6+(con_local[[2]]-1)*6), (1+(con_local[[2]]-1)*6):(6+(con_local[[2]]-1)*6)] + k_local_4
+    
+    k_local_5 <- k_local[13:18,13:18]
+
+    k_global[(1+(con_local[[3]]-1)*6):(6+(con_local[[3]]-1)*6), (1+(con_local[[3]]-1)*6):(6+(con_local[[3]]-1)*6)] =
+        k_global[(1+(con_local[[3]]-1)*6):(6+(con_local[[3]]-1)*6), (1+(con_local[[3]]-1)*6):(6+(con_local[[3]]-1)*6)] + k_local_5
+        
+    k_local_6 <- k_local[13:18,19:24]
+
+    k_global[(1+(con_local[[3]]-1)*6):(6+(con_local[[3]]-1)*6), (1+(con_local[[4]]-1)*6):(6+(con_local[[4]]-1)*6)] =
+        k_global[(1+(con_local[[3]]-1)*6):(6+(con_local[[3]]-1)*6), (1+(con_local[[4]]-1)*6):(6+(con_local[[4]]-1)*6)] + k_local_6
+
+    k_local_7 <- k_local[19:24,13:18]
+
+    k_global[(1+(con_local[[4]]-1)*6):(6+(con_local[[4]]-1)*6), (1+(con_local[[3]]-1)*6):(6+(con_local[[3]]-1)*6)] =
+        k_global[(1+(con_local[[4]]-1)*6):(6+(con_local[[4]]-1)*6), (1+(con_local[[3]]-1)*6):(6+(con_local[[3]]-1)*6)] + k_local_7
+
+    k_local_8 <- k_local[19:24,19:24]
+
+    k_global[(1+(con_local[[4]]-1)*6):(6+(con_local[[4]]-1)*6), (1+(con_local[[4]]-1)*6):(6+(con_local[[4]]-1)*6)] =
+        k_global[(1+(con_local[[4]]-1)*6):(6+(con_local[[4]]-1)*6), (1+(con_local[[4]]-1)*6):(6+(con_local[[4]]-1)*6)] + k_local_8
+        
+    b_local_1 <- b_local[1:6,]
+
+    b_global[(1+(con_local[[1]]-1)*6):(6+(con_local[[1]]-1)*6),] = b_global[(1+(con_local[[1]]-1)*6):(6+(con_local[[1]]-1)*6),] + b_local_1
+    
+    b_local_2 <- b_local[7:12,]
+
+    b_global[(1+(con_local[[2]]-1)*6):(6+(con_local[[2]]-1)*6),] = b_global[(1+(con_local[[2]]-1)*6):(6+(con_local[[2]]-1)*6),] + b_local_2
+
+}
+
+end.time <- Sys.time()
+time.taken <- end.time - start.time
+print(time.taken)
+print(object.size(k_global))
+print(object.size(b_global))
---
+++ python/navierstokes.py
@@ -0,0 +1,105 @@
+import sympy as sy
+import time
+
+#Symbols to be used
+firstPart = "result.txt"
+secondPart = "result2.txt"
+
+x, y, z, t = sy.var('x y z t')
+
+fx, fy, fz = sy.var('fx fy fz')
+
+x1, x2, x3, x4 = sy.var('x1 x2 x3 x4')
+y1, y2, y3, y4 = sy.var('y1 y2 y3 y4')
+z1, z2, z3, z4 = sy.var('z1 z2 z3 z4')
+
+a, b, c, d, e, f = sy.var('a b c d e f')
+
+vx1, vy1, vz1 = sy.var('vx1 vy1 vz1')
+vx2, vy2, vz2 = sy.var('vx2 vy2 vz2')
+vx3, vy3, vz3 = sy.var('vx3 vy3 vz3')
+vx4, vy4, vz4 = sy.var('vx4 vy4 vz4')
+
+p1, p2, p3, p4 = sy.var('p1 p2 p3 p4')
+
+advection = sy.var('advection')
+density = sy.var('density')
+
+#Main function, get constants and send them to every function in charge of one of the equation's terms
+def navier_stokes():
+    ''' Ni stands for N integrated '''
+    Ni = sy.Matrix([[-1/2,0,0,1/2,0,0,1/2,0,0,1/2,0,0],
+                     [0,-1/2,0,0,1/2,0,0,1/2,0,0,1/2,0],
+                     [0,0,-1/2,0,0,1/2,0,0,1/2,0,0,1/2]])
+    
+    ''' Nti stands for N integrated and transposed'''
+    Nti = Ni.T
+    
+    force = sy.Matrix([fx, fy, fz])
+    
+    D = sy.Matrix([[(x2-x1),(x3-x1),(x4-x1)],[(y2-y1),(y3-y1),(y4-y1)],[(z2-z1),(z3-z1),(z4-z1)]]).det()
+
+    '''deltaN is 3x12'''
+    deltaN = sy.Matrix([[(x2-x1),(y2-y1),(z2-z1)],[(x3-x1),(y3-y1),(z3-z1)],[(x4-x1),(y4-y1),(z4-z1)]]).inv()*sy.Matrix([[-1,-1,-1,1,1,1,0,0,0,0,0,0],
+                        [-1,-1,-1,0,0,0,1,1,1,0,0,0],
+                        [-1,-1,-1,0,0,0,0,0,0,1,1,1]])
+    V = sy.Matrix([vx1, vy1, vz1, vx2, vy2, vz2, vx3, vy3, vz3, vx4, vy4, vz4])
+
+    P = sy.Matrix([p1/3, p1/3, p1/3, p2/3, p2/3, p2/3, p3/3, p3/3, p3/3, p4/3, p4/3, p4/3])
+
+    '''Calculating each part of the locale equation'''
+    res = sy.zeros(24)
+    incognitas = sy.Matrix([V,P])
+    
+    res[0:12, 0:12] = advection_part(D, Nti, advection, deltaN)+double_delta_part(deltaN, sy.Matrix([x1, x2, x3, x4]),sy.Matrix([y1, y2, y3, y4]),sy.Matrix([z1, z2, z3, z4]))+last_part(D, Nti, deltaN)
+    res[12:24,12:24] = density_part(density,D,Nti,deltaN)
+    
+    '''Analysis done with velocity only'''
+    partB = V+(t*force_part(Nti,force,D))
+
+    force_matrix = force_part(Nti, force, D)
+    
+    output = open(firstPart, 'w')
+    output.write(str(res)+'*'+str(incognitas))
+    output.write('\n')
+    output.write(str(force_matrix))
+    output.close()
+    
+    output = open(secondPart, 'w')
+    output.write(str(partB))
+    output.close()
+    print("Done")
+
+
+#Nti = N, tranposed and integrated
+def force_part(Nti, force, D):
+    print("force")
+    res = D*Nti*force
+    return res
+    
+def advection_part(D, Nti, advection, deltaN):
+    print("advection")
+    res = advection*D*Nti*deltaN
+    return res 
+
+def density_part(density, D, Nti, deltaN):
+    print("density")
+    res = (D/density)*Nti*deltaN
+    return res
+
+def double_delta_part(deltaN, X, Y, Z):
+    print("doubledelta")
+    '''Definition of a,b,c,d,e,f will be done in R'''
+    res = (b-a)*(d-c)*(f-e)*deltaN.transpose()*deltaN
+    return res
+
+def last_part(D, Nti, deltaN):
+    print("last")
+    res = D*Nti*deltaN
+    return res
+print("Ejecutando sin optimizaciones")
+start_time = time.time()
+navier_stokes()
+end_time = time.time()
+print("--- Tiempo total: %s segundos ---" % (end_time - start_time))
+
